8951df2e59ce989c3efbfbb3a2ebec8e259f0096
antoniolg <antonio@devexperto.com>
2023-04-03 16:14:20 +0200

    Publishing configuration

--- customPlugin/build.gradle.kts
+++ customPlugin/build.gradle.kts
@@ -1,19 +1,36 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.1.0"
 }
 
-repositories {
-    mavenCentral()
+group = "com.devexperto"
+version = "1.0"
+
+pluginBundle {
+    website = "https://devexperto.com"
+    vcsUrl = "https://github.com/devexperto/gradle-training"
+    tags = listOf("gradle", "plugin", "android", "kotlin")
 }
 
 gradlePlugin {
 
     plugins {
         create("custom-plugin") {
-            id = "custom-plugin"
+            id = "com.devexperto.customplugin"
             implementationClass = "com.devexperto.customplugin.CustomPlugin"
+            displayName = "Custom-Plugin"
+            description = "A plugin that does nothing and serves no purpose"
         }
     }
 
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "localPluginRepository"
+            url = uri("../local-plugin-repository")
+        }
+    }
 }
